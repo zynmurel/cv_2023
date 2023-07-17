d020b1c44e082dca6cf0fface2fe98fbd720f561
zynmurel <comingues99@gmail.com>
2023-07-17 22:20:51 +0800

    edit details

--- src/Pages/Components/Infos.jsx
+++ src/Pages/Components/Infos.jsx
@@ -1,224 +1,290 @@
 import { Box } from "@mui/system";
-import { DiJavascript1, DiHtml5, DiCss3, DiPhotoshop, DiReact } from "react-icons/di";
+import {
+  DiJavascript1,
+  DiHtml5,
+  DiCss3,
+  DiPhotoshop,
+  DiReact,
+} from "react-icons/di";
 import { FaLaravel, FaPhoneAlt } from "react-icons/fa";
 import { FcOpenedFolder } from "react-icons/fc";
+import { SiNextdotjs, SiPhp } from "react-icons/si";
+
 import { BsGithub } from "react-icons/bs";
 
 const Infos = () => {
-    //const skills = ['JAVASCRIPT', 'HTML', 'CSS', 'REACT', 'PHOTOSHOP', 'LARAVEL'];
-    const skills = [
-        {
-            title:'JAVASCRIPT',
-            color:'yellow',
-            fontSize:'25px',
-            percent:'80%'
-        },{
-            title:'HTML',
-            color:"rgb(255, 145, 0)",
-            fontSize:'25px',
-            percent:'85%'
-        },{
-            title:'CSS',
-            color:"rgb(45, 189, 255)",
-            fontSize:'25px',
-            percent:'80%'
-        },{
-            title:'REACT',
-            color:"rgb(45, 189, 255)",
-            fontSize:'25px',
-            percent:'85%'
-        },{
-            title:'REACT NATIVE',
-            color:"rgb(45, 189, 255)",
-            fontSize:'25px',
-            percent:'60%'
-        },
-        {
-            title:'LARAVEL',
-            color:'red',
-            fontSize:'18px',
-            percent:'50%'
-        },{
-            title:'PHOTOSHOP',
-            color:"rgb(45, 189, 255)",
-            fontSize:'25px',
-            percent:'80%'
-        }
-    ]
-    const education = [
-        {
-            school:'BOHOL ISLAND STATE UNIVERSITY',
-            address:'BS Information Technology / Magsija, Balilihan, Bohol'
-        },
-        {
-            school:'BILAR NATIONAL HIGH SCHOOL',
-            address:'Yanaya, Bilar, Bohol'
-        },
-        {
-            school:'BILAR CENTRAL ELEMENTARY SCHOOL',
-            address:'Poblacion, Bilar, Bohol'
-        }
-    ]
-    const skillsList = (skillArr) => ( 
-        skillArr.map(sk => {
-            return (
-            
-            <div key={sk.title}>
-                <div className="skl">
-                            {sk.title === 'LARAVEL' && <FaLaravel color={sk.color} fontSize={sk.fontSize}/>}
-                            {sk.title === 'PHOTOSHOP' && <DiPhotoshop color={sk.color} fontSize={sk.fontSize}/>}
-                            {sk.title === 'REACT' && <DiReact color={sk.color} fontSize={sk.fontSize}/>}
-                            {sk.title === 'REACT NATIVE' && <DiReact color={sk.color} fontSize={sk.fontSize}/>}
-                            {sk.title === 'JAVASCRIPT' && <DiJavascript1 color={sk.color} fontSize={sk.fontSize}/>}
-                            {sk.title === 'CSS' && <DiCss3 color={sk.color} fontSize={sk.fontSize}/>}
-                            {sk.title === 'HTML' && <DiHtml5 color={sk.color} fontSize={sk.fontSize}/>}
-                            <h4>{sk.title}</h4>
-                        </div>
-                        <div className="percentbox">
-                            <div className="percent" style={{ width:sk.percent }}></div>
-                        </div>
+  //const skills = ['JAVASCRIPT', 'HTML', 'CSS', 'REACT', 'PHOTOSHOP', 'LARAVEL'];
+  const skills = [
+    {
+      title: "JAVASCRIPT",
+      color: "yellow",
+      fontSize: "25px",
+      percent: "80%",
+    },
+    {
+      title: "PHP",
+      color: "rgba(78,92,147)",
+      fontSize: "25px",
+      percent: "65%",
+    },
+    {
+      title: "HTML",
+      color: "rgb(255, 145, 0)",
+      fontSize: "25px",
+      percent: "85%",
+    },
+    {
+      title: "CSS",
+      color: "rgb(45, 189, 255)",
+      fontSize: "25px",
+      percent: "80%",
+    },
+    {
+      title: "REACT",
+      color: "rgb(45, 189, 255)",
+      fontSize: "25px",
+      percent: "85%",
+    },
+    {
+      title: "NEXTJS",
+      color: "white",
+      fontSize: "25px",
+      percent: "45%",
+    },
+    {
+      title: "REACT NATIVE",
+      color: "rgb(45, 189, 255)",
+      fontSize: "25px",
+      percent: "60%",
+    },
+    {
+      title: "LARAVEL",
+      color: "red",
+      fontSize: "18px",
+      percent: "50%",
+    },
+    {
+      title: "PHOTOSHOP",
+      color: "rgb(45, 189, 255)",
+      fontSize: "25px",
+      percent: "80%",
+    },
+  ];
+  const education = [
+    {
+      school: "BOHOL ISLAND STATE UNIVERSITY",
+      address: "BS Information Technology / Magsija, Balilihan, Bohol",
+      year: "2019-2023",
+    },
+    {
+      school: "BILAR NATIONAL HIGH SCHOOL",
+      address: "Yanaya, Bilar, Bohol",
+      year: "2011-2015",
+    },
+    {
+      school: "BILAR CENTRAL ELEMENTARY SCHOOL",
+      address: "Poblacion, Bilar, Bohol",
+      year: "2005-2011",
+    },
+  ];
+  const skillsList = (skillArr) =>
+    skillArr.map((sk) => {
+      return (
+        <div key={sk.title}>
+          <div className="skl">
+            {sk.title === "LARAVEL" && (
+              <FaLaravel color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "PHOTOSHOP" && (
+              <DiPhotoshop color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "REACT" && (
+              <DiReact color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "REACT NATIVE" && (
+              <DiReact color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "JAVASCRIPT" && (
+              <DiJavascript1 color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "CSS" && (
+              <DiCss3 color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "HTML" && (
+              <DiHtml5 color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "NEXTJS" && (
+              <SiNextdotjs color={sk.color} fontSize={sk.fontSize} />
+            )}
+            {sk.title === "PHP" && (
+              <SiPhp color={sk.color} fontSize={sk.fontSize} />
+            )}
+            <h4>{sk.title}</h4>
+          </div>
+          <div className="percentbox">
+            <div className="percent" style={{ width: sk.percent }}></div>
+          </div>
+        </div>
+      );
+    });
+  const educationList = (eduArr) =>
+    eduArr.map((edu) => (
+      <div className="org">
+        <h2 className="orgtitle">{edu.school}</h2>
+        <div className="posint">
+          <p className="position" style={{ fontStyle: "italic" }}>
+            {edu.address}
+          </p>
+          <p style={{ marginTop: 0, marginBottom: 5 }}>{edu.year}</p>
+        </div>
+      </div>
+    ));
+  return (
+    <Box className="infos">
+      <div className="info2">
+        <div className="info2margin">
+          <div className="title">
+            <h1 className="exp">EXPERIENCE</h1>
+          </div>
+
+          <div className="org">
+            <h2 className="year">2021-2022</h2>
+            <h2 className="orgtitle">GRAPHIC DESIGN</h2>
+            <div className="posint">
+              <ul>
+                <li>
+                  Worked with school organization ( SSG and Campus Access ) and
+                  created images and layouts using Adobe Photoshop.
+                </li>
+              </ul>
             </div>
-        )})
-    )
-    const educationList = (eduArr) => (
-        eduArr.map(edu => (
-            <div className="org">
-                <h2 className="orgtitle">{edu.school}</h2>
-                <div className="posint">
-                    <p className="position" style={{  fontStyle:'italic' }}>
-                        {edu.address}
-                    </p>
-                </div>
+          </div>
+
+          <div className="org">
+            <h2 className="year">2021-2022</h2>
+            <h2 className="orgtitle">SOFTWARE DEVELOPMENT</h2>
+            <div className="posint">
+              <ul>
+                <li>
+                  Developed a Web Application for Water Billing System using
+                  React (Front-end) and Laravel (Back-end) as part of our
+                  Capstone Project and Research.
+                </li>
+                <li>
+                  Gained practical experience in Web development during an
+                  internship focused on Digital Transformation.
+                </li>
+                <li>
+                  Collaborated with fellow interns to create the OJT Attendance
+                  Web System using React and GraphQL for our final group
+                  project.
+                </li>
+              </ul>
             </div>
-        ))
-    )
-    return ( 
-        <Box className='infos'>
-            <div className="info2">
-            <div className="info2margin">
-                <div className="title">
-                    <h1 className="exp">
-                        EXPERIENCE
-                    </h1>
+          </div>
+
+          <div className="title">
+            <h1 className="exp">EDUCATION</h1>
+          </div>
+          {educationList(education)}
+        </div>
+        <div className="info2margin">
+          <div className="org">
+            <h2 className="orgtitleproject">WEB DEVELOPMENT PROJECTS</h2>
+            <div className="skl" style={{ margin: "0 0 5px 0", fontSize: 15 }}>
+              <BsGithub fontSize={"21px"} />
+              <h4 style={{ margin: "0 0 0 5px" }}>Github Repositories:</h4>
+            </div>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/zynmurel/DOTA2-HTML-CSS"
+                  target="_blank"
+                >
+                  DOTA 2 ( HTML / CSS )
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/zynmurel/DOTA2-ReactApp"
+                  target="_blank"
+                >
+                  DOTA 2 ( React )
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/zynmurel/TodoList-ReactNative"
+                  target="_blank"
+                >
+                  Todo List ( React Native )
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/zynmurel/waterBilling1"
+                  target="_blank"
+                >
+                  Water Billing App Frontend ( React )
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/zynmurel/waterbillingBackendv2"
+                  target="_blank"
+                >
+                  Water Billing App Backend ( Laravel )
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/zynmurel/WaterMeterReader"
+                  target="_blank"
+                >
+                  Water Billing Mobile App ( React Native )
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="info1">
+        <div className="line"></div>
+        <div className="contacts">
+          <div className="title">
+            <div className="color"></div>
+            <h1>PERSONAL INFO</h1>
+          </div>
+          <div className="skillsinfo">
+            <div className="pinfo" style={{ marginTop: "20px" }}>
+              <h3>ADDRESS</h3>
+              <p>Poblacion, Bilar, Bohol</p>
             </div>
 
-            <div className="org">
-                <h2 className="year">2021-2022</h2>
-                <h2 className="orgtitle">GRAPHIC DESIGN</h2>
-                <div className="posint">
-                    <p className="position">
-                    Worked with school organization ( SSG and Campus Access ) and created images and layouts using Adobe Photoshop.
-                    </p>
-                </div>
+            <div className="pinfo" style={{ marginTop: "20px" }}>
+              <h3>PHONE</h3>
+              <p>+639307038821</p>
             </div>
 
-            <div className="org">
-                <h2 className="year">2021-2022</h2>
-                <h2 className="orgtitle">SOFTWARE DEVELOPMENT</h2>
-                <div className="posint">
-                    <p className="position">
-                    Undergone Software Development training in Vijaya Inc and currently creating our Web App for our Capstone Project and Research.
-                    </p>
-                </div>
+            <div className="pinfo" style={{ marginTop: "20px" }}>
+              <h3>EMAIL</h3>
+              <p>comingues99@gmail.com</p>
             </div>
+          </div>
+        </div>
+        <div className="line"></div>
+        <div className="skills">
+          <div className="title">
+            <div className="color"></div>
+            <h1>SKILLS</h1>
+          </div>
+          <div className="skillsinfo">{skillsList(skills)}</div>
+        </div>
+        <div className="line"></div>
+      </div>
+    </Box>
+  );
+};
 
-            <div className="title">
-                    <h1 className="exp">
-                        EDUCATION
-                    </h1>
-            </div>
-            {
-                educationList(education)
-            }
-                </div>
-                <div className="info2margin">
-                <div className="title">
-                    <h1 className="exp">
-                        PROJECTS
-                    </h1>
-            </div>
-
-            <div className="org">
-                <h2 className="orgtitleproject">GRAPHIC DESIGN PROJECTS</h2>
-                <div className="skl" style={{ margin:'0 0 20px 0', fontSize:15 }}>
-                    <FcOpenedFolder fontSize={"22px"}/>
-                    <h4 style={{ margin:'0 0 0 5px' }}><a href="https://drive.google.com/drive/folders/1ca1HyLKhJdjFR9kzdWH8W08cIO3XMVpZ?usp=share_link" target='_blank'>Graphic Design Projects</a></h4>
-                </div>
-            </div>
-
-            <div className="org">
-                <h2 className="orgtitleproject">SOFTWARE DEVELOPMENT PROJECTS</h2>
-                <div className="skl" style={{ margin:'0 0 5px 0', fontSize:15 }}>
-                    <BsGithub fontSize={"21px"}/>
-                    <h4 style={{ margin:'0 0 0 5px' }}>Github Repositories:</h4>
-                </div>
-                    <ul>
-                        <li><a href="https://github.com/zynmurel/DOTA2-HTML-CSS" target='_blank'>DOTA 2 ( HTML / CSS )</a></li>
-                        <li><a href="https://github.com/zynmurel/DOTA2-ReactApp" target='_blank'>DOTA 2 ( React )</a></li>
-                        <li><a href="https://github.com/zynmurel/TodoList-ReactNative" target='_blank'>Todo List ( React Native )</a></li>
-                        <li><a href="https://github.com/zynmurel/waterBilling1" target='_blank'>Water Billing App Frontend ( React )</a></li>
-                        <li><a href="https://github.com/zynmurel/waterbillingBackendv2" target='_blank'>Water Billing App Backend ( Laravel )</a></li>
-                        <li><a href="https://github.com/zynmurel/WaterMeterReader" target='_blank'>Water Billing Mobile App ( React Native )</a></li>
-                    </ul>
-            </div>
-
-                </div>
-                
-            </div>
-
-            
-            <div className="info1">
-                <div className="line"></div>
-                <div className="contacts">
-                    <div className="title">
-                    <div className="color">
-                        
-                    </div>
-                    <h1>
-                        PERSONAL INFO
-                    </h1>
-                    </div>
-                    <div className="skillsinfo">
-
-                        <div className="pinfo" style={{ marginTop: '20px' }}>
-                            <h3>ADDRESS</h3>
-                            <p>Poblacion, Bilar, Bohol</p>
-                        </div>
-
-                        <div className="pinfo" style={{ marginTop: '20px' }}>
-                            <h3>PHONE</h3>
-                            <p>+639307038821</p>
-                        </div>
-
-                        <div className="pinfo" style={{ marginTop: '20px' }}>
-                            <h3>EMAIL</h3>
-                            <p>comingues99@gmail.com</p>
-                        </div>
-                    </div>
-                    
-
-                </div>
-                <div className="line"></div>
-                <div className="skills">
-                    <div className="title">
-                    <div className="color">
-
-                    </div>
-                    <h1>
-                        SKILLS
-                    </h1>
-                    </div>
-                    <div className="skillsinfo">
-                    {skillsList(skills)}
-                    </div>
-
-                </div>
-                <div className="line"></div>
-                
-                
-            </div>
-        </Box>
-     );
-}
- 
 export default Infos;
--- src/Pages/Components/Self.jsx
+++ src/Pages/Components/Self.jsx
@@ -1,29 +1,28 @@
 import { Box } from "@mui/system";
 
 const Self = () => {
-    return ( 
-        <Box className='self'>
-        <img src="/12.png" alt="selfpic" className="selfpic"/>
-        <div className="intro">
-            <div className="intro1">
-                <h1 >
-                    SEAN MARUEL
-                </h1>
-                <h1 >COMINGUES</h1>
-                <h5>
-                    BS Information Technology Student
-                </h5>
-            </div>
-            <div className="intro2">
-                <div className="introduction">
-                    <p>
-                    I am a 4th Year, Bachelor of Science in Information Technology Student. I want to explore more about programming and also to enhance my skills and become successful in the future. 
-                    </p>
-                </div>
-            </div>
+  return (
+    <Box className="self">
+      <img src="/12.png" alt="selfpic" className="selfpic" />
+      <div className="intro">
+        <div className="intro1">
+          <h1>SEAN MARUEL</h1>
+          <h1>COMINGUES</h1>
+          <h5>Aspirining Web Developer</h5>
         </div>
-        </Box>
-     );
-}
- 
+        <div className="intro2">
+          <div className="introduction">
+            <p>
+              I recently graduated with a Bachelor of Science degree in
+              Information Technology and have gained practical experience in web
+              development. I am eager to further enhance my coding skills for
+              future endeavors.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Box>
+  );
+};
+
 export default Self;
